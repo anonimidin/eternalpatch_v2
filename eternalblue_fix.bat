2fbb9935fcd8432bb640469f95cb571fe0f0d6cf
r3x08 <109206637+r3x08@users.noreply.github.com>
2023-02-07 00:04:44 -0800

    Update eternalblue_fix.bat

--- eternalblue_fix.bat
+++ eternalblue_fix.bat
@@ -1,44 +1,114 @@
 @echo off
-color F9
-title ETERNALBLUE Fix @tr34x0r
+:: Console size 70x25
+mode 70,25
+:: Color Blue & White text
+color 1f
+:: UTF-8 unicode
+chcp 65001 >nul
+:: Title
+title EternalBlue Defense
 
-ver | findstr /i "6.1" > nul
-if %errorlevel% == 0 (
-  echo Windows 7 detected
-  echo Checking if ports 139 and 445 are open...
-  netstat -an | find "139" > nul
-  if %errorlevel% == 0 (
-    echo Port 139 is vulnerable to potential security threats, blocking incoming connections...
-    netsh advfirewall firewall add rule name="Block TCP 139" dir=in action=block protocol=TCP localport=139
-  )
-  netstat -an | find "445" > nul
-  if %errorlevel% == 0 (
-    echo Port 445 is vulnerable to potential security threats, blocking incoming connections...
-    netsh advfirewall firewall add rule name="Block TCP 445" dir=in action=block protocol=TCP localport=445
-  )
-)
+:menu
+    echo Main Menu:
+    echo.
+    echo 1. EternalBlue protect
+    echo 2. SMBv1 to SMBv2
+    echo 3. About
+    echo 4. Restart PC
+    echo 5. Exit
+    echo.
+    set choice=
+    set /p choice=Enter your choice: 
+    ::Choices with calling functions
+    if %choice% == 1 ( call :fix )
+    if %choice% == 2 ( call :update )
+    if %choice% == 3 ( start https://github.com/cyberdome-tj )
+    if %choice% == 4 ( call :restartPC )
+    if %choice% == 5 ( msg * " Obeserve ur security! CyberDome " && exit )
+goto menu
 
-ver | findstr /i "6.3" > nul
-if %errorlevel% == 0 (
-  echo Windows 8.1 detected
-  echo Checking if ports 139 and 445 are open...
-  netstat -an | find "139" > nul
-  if %errorlevel% == 0 (
-    echo Port 139 is vulnerable to potential security threats, blocking incoming connections...
-    netsh advfirewall firewall add rule name="Block TCP 139" dir=in action=block protocol=TCP localport=139
-  )
-  netstat -an | find "445" > nul
-  if %errorlevel% == 0 (
-    echo Port 445 is vulnerable to potential security threats, blocking incoming connections...
-    netsh advfirewall firewall add rule name="Block TCP 445" dir=in action=block protocol=TCP localport=445
-  )
-)
+:fix
+    echo Checking Windows version...
+    ver | findstr /i "not found" > nul
+    if %errorlevel% == 0 (
+    echo Windows version is not supported or not recognized
+    echo No action taken. Exiting.
+    exit
+    )
+    ::Windows 7
+    echo Adding firewall rules to block incoming connections on vulnerable ports...
+    ver | findstr /i "6.1" > nul
+    if %errorlevel% == 0 (
+    echo.
+    echo Windows 7 detected
+    echo Blocking incoming connections on ports 139 and 445...
+    netsh advfirewall firewall show rule name="Block TCP 139" > nul
+    if %errorlevel% == 0 (
+        echo Port 139 already blocked
+    ) else (
+        netsh advfirewall firewall add rule name="Block TCP 139" dir=in action=block protocol=TCP localport=139
+        echo Port 139 blocked
+    )
 
-ver | findstr /i "not found" > nul
-if %errorlevel% == 0 (
-  echo Windows version is not vulnerable
-  echo No action taken
-  exit
-)
+    netsh advfirewall firewall show rule name="Block TCP 445" > nul
+    if %errorlevel% == 0 (
+        echo Port 445 already blocked
+    ) else (
+        netsh advfirewall firewall add rule name="Block TCP 445" dir=in action=block protocol=TCP localport=445
+        echo Port 445 blocked
+    )
+    echo PRESS ENTER TO RETURN TO MAIN MENU. && pause > nul && cls
+    )
+    ::Windows 8.1
+    ver | findstr /i "6.3" > nul
+    if %errorlevel% == 0 (
+    cls && echo.
+    echo Windows 8.1 detected
+    echo Blocking incoming connections on ports 139 and 445...
+    netsh advfirewall firewall show rule name="Block TCP 139" > nul
+    if %errorlevel% == 0 (
+        echo Port 139 already blocked
+    ) else (
+        netsh advfirewall firewall add rule name="Block TCP 139" dir=in action=block protocol=TCP localport=139
+        echo Port 139 blocked
+    )
 
-pause
+    netsh advfirewall firewall show rule name="Block TCP 445" > nul
+    if %errorlevel% == 0 (
+        echo Port 445 already blocked
+    ) else (
+        netsh advfirewall firewall add rule name="Block TCP 445" dir=in action=block protocol=TCP localport=445
+        echo Port 445 blocked
+    )
+    echo PRESS ENTER TO RETURN TO MAIN MENU. && pause >nul && cls
+    )
+    ::Windows 10
+    ver | findstr /i "10." > nul
+    if %errorlevel% == 0 (
+    cls && echo.
+    echo Windows 10 detected
+    echo Blocking incoming connections on port 445...
+    netsh advfirewall firewall show rule name="Block TCP 445" > nul
+    if %errorlevel% == 0 (
+        echo Port 445 already blocked
+    ) else (
+        netsh advfirewall firewall add rule name="Block TCP 445" dir=in action=block protocol=TCP localport=445
+        echo Port 445 blocked
+    )
+    echo PRESS ENTER TO RETURN TO MAIN MENU. && pause >nul && cls
+    )
+goto menu 
+
+:update
+    ::Powershell commands changes SMBv1 to SMBv2.
+    color f1 && powershell.exe Set-SmbServerConfiguration -EnableSMB1Protocol $false -Confirm:$false; Set-SmbServerConfiguration -EnableSMB2Protocol $true -Confirm:$false
+    echo.
+    echo SMBv1 changed to SMBv2! Restart PC!
+    echo PRESS ENTER TO RETURN TO MAIN MENU. && pause >nul && cls
+goto menu
+
+:restartPC
+    ::Restart the computer
+    echo Restarting the PC...
+    shutdown /r /c "Observe your security!" /t 3
+goto menu
